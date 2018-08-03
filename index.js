7afc2b72c7d1e2289a61c6b245bb15c235e261fe
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-03 14:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (blockNumber) {
-  if (blockNumber > 42) {
-    return blockNumber - 42;
-  } else {
-    return 42 - blockNumber;
-  }
+ 
+    return Math.abs(blockNumber - 42);
+ 
 }
