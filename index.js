8fdc183b3f949c947937939b35c6feb34dd6c204
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-03 14:06:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blockNumber) {
+ 
+    return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(destination - start) * 264;
+ 
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance-400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
